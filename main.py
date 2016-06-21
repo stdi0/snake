c47b319cbab07f620c9f5eafb2ae0dfc87271485
Denis <denisyurievich94@gmail.com>
2016-06-21 18:59:23 +0300

    Object version, added background, minor fixes

--- main.py
+++ main.py
@@ -2,114 +2,114 @@ from tkinter import *
 from time import *
 from random import *
 
-WIDTH = 500
-HEIGHT = 500
-directions = []
-directions.append((0,-20))
+class SnakeGame:
 
-def check_game_over():
-    for elem in snake_body:
-        if canv.coords(snake_head) == canv.coords(elem):
-            return True
-    return False
+    WIDTH = 500
+    HEIGHT = 500
 
-def spawn_food():
-    global apple
-    x1 = choice([i for i in range(0,500,20)])
-    y1 = choice([i for i in range(0,500,20)])
-    x2 = x1 + 20
-    y2 = y1 + 20
-    apple = canv.create_oval([x1,y1],[x2,y2], fill="white")
-
-def food_inspection():
-    if canv.coords(snake_head) == canv.coords(apple):
-        return True
-    else:
+    def check_game_over(self):
+        for elem in self.snake_body:
+            if self.canv.coords(self.snake_head) == self.canv.coords(elem):
+                self.canv.create_text(250,250, text="Game Over",
+                    font="Verdana 18", justify=CENTER, fill="red")
+                return True
         return False
 
-def change_xy(event):
-    print(event.keysym)
-    if event.keysym == 'Right' and directions[-1][0] != -20:
-            x = 20
-            y = 0
-            directions.append((x,y))
-    if event.keysym == 'Left' and directions[-1][0] != 20:
-            x = -20
-            y = 0
-            directions.append((x,y))
-    if event.keysym == 'Up' and directions[-1][1] != 20:
-            x = 0
-            y = -20
-            directions.append((x,y))
-    if event.keysym =='Down' and directions[-1][1] != -20:
-            x = 0
-            y = 20
-            directions.append((x,y))
+    def spawn_food(self):
+        x1 = choice([i for i in range(0,500,20)])
+        y1 = choice([i for i in range(0,500,20)])
+        x2 = x1 + 20
+        y2 = y1 + 20
+        for elem in self.snake_body:
+            if self.canv.coords(elem) == [x1,y1,x2,y2]:
+                return self.spawn_food()
+        self.apple = self.canv.create_oval([x1,y1],[x2,y2], fill="#FFFF00")
 
-def go():
-   #tmp = (0,-20)
-   i = 0
-   while True:
-        try:
-            x, y = directions[i]
-            tmp = (x, y)
-            i += 1
-        except:
-            x, y = tmp
-        prev_coords = canv.coords(snake_head)
-        canv.move(snake_head,x,y)
-        print(canv.coords(snake_head))
-        if canv.coords(snake_head)[1] < 0:
-            x1,y1,x2,y2 = canv.coords(snake_head)
-            y1, y2 = HEIGHT-20, HEIGHT
-            canv.coords(snake_head,x1,y1,x2,y2)
-        if canv.coords(snake_head)[3] > 500:
-            x1,y1,x2,y2 = canv.coords(snake_head)
-            y1, y2 = 0, 20
-            canv.coords(snake_head,x1,y1,x2,y2)
-        if canv.coords(snake_head)[2] > 500:
-            x1,y1,x2,y2 = canv.coords(snake_head)
-            x1, x2 = 0, 20
-            canv.coords(snake_head,x1,y1,x2,y2)
-        if canv.coords(snake_head)[0] < 0:
-            x1,y1,x2,y2 = canv.coords(snake_head)
-            x1, x2 = WIDTH-20, WIDTH
-            canv.coords(snake_head,x1,y1,x2,y2)    
+    def food_inspection(self):
+        if self.canv.coords(self.snake_head) == self.canv.coords(self.apple):
+            return True
+        else:
+            return False
 
-        for k, elem in enumerate(snake_body):
-            x1,y1,x2,y2 = prev_coords
-            prev_coords = canv.coords(snake_body[k])
-            canv.coords(snake_body[k],x1,y1,x2,y2)
-        
-        if check_game_over():
-            break
+    def change_xy(self, event):
+        print(event.keysym)
+        if event.keysym == 'Right' and self.directions[-1][0] != -20:
+                x = 20
+                y = 0
+                self.directions.append((x,y))
+        if event.keysym == 'Left' and self.directions[-1][0] != 20:
+                x = -20
+                y = 0
+                self.directions.append((x,y))
+        if event.keysym == 'Up' and self.directions[-1][1] != 20:
+                x = 0
+                y = -20
+                self.directions.append((x,y))
+        if event.keysym =='Down' and self.directions[-1][1] != -20:
+                x = 0
+                y = 20
+                self.directions.append((x,y))
 
-        snake_tail = prev_coords
-        if food_inspection():
-            canv.delete(apple)
-            x1,y1,x2,y2 = snake_tail
-            snake_body.append(canv.create_oval([x1,y1],[x2,y2], fill="white"))
-            spawn_food()
-        #canv.after(350,go)
-        canv.update()
-        sleep(0.2)
+    def go(self):
+       i = 0
+       while True:
+            try:
+                x, y = self.directions[i]
+                tmp = (x, y)
+                i += 1
+            except IndexError:
+                x, y = tmp
+            prev_coords = self.canv.coords(self.snake_head)
+            self.canv.move(self.snake_head,x,y)
 
+            if self.canv.coords(self.snake_head)[1] < 0:
+                x1,y1,x2,y2 = self.canv.coords(self.snake_head)
+                y1, y2 = self.HEIGHT-20, self.HEIGHT
+                self.canv.coords(self.snake_head,x1,y1,x2,y2)
+            if self.canv.coords(self.snake_head)[3] > 500:
+                x1,y1,x2,y2 = self.canv.coords(self.snake_head)
+                y1, y2 = 0, 20
+                self.canv.coords(self.snake_head,x1,y1,x2,y2)
+            if self.canv.coords(self.snake_head)[2] > 500:
+                x1,y1,x2,y2 = self.canv.coords(self.snake_head)
+                x1, x2 = 0, 20
+                self.canv.coords(self.snake_head,x1,y1,x2,y2)
+            if self.canv.coords(self.snake_head)[0] < 0:
+                x1,y1,x2,y2 = self.canv.coords(self.snake_head)
+                x1, x2 = self.WIDTH-20, self.WIDTH
+                self.canv.coords(self.snake_head,x1,y1,x2,y2)    
+
+            for k, elem in enumerate(self.snake_body):
+                x1,y1,x2,y2 = prev_coords
+                prev_coords = self.canv.coords(self.snake_body[k])
+                self.canv.coords(self.snake_body[k],x1,y1,x2,y2)
+            
+            if self.check_game_over():
+                break
+
+            if self.food_inspection():
+                self.canv.delete(self.apple)
+                x1,y1,x2,y2 = prev_coords
+                self.snake_body.append(self.canv.create_oval([x1,y1],[x2,y2], fill="white"))
+                self.spawn_food()
+            
+            self.canv.update()
+            sleep(0.2)
+
+    def __init__(self):
+        self.canv = Canvas(root, width=self.WIDTH, height=self.HEIGHT, bg="#003300")
+        self.canv.pack()
+        self.im ='/Users/irina_dashevskaya/project_2/background.gif'
+        self.ph_im =PhotoImage(file=self.im)
+        self.canv.create_image(1,1,anchor=NW,image=self.ph_im)
+        self.snake_head = self.canv.create_oval([240,240],[260,260], fill="white")
+        self.snake_body = []
+        self.directions = [(0,-20)]
+        self.spawn_food()
+        root.bind("<KeyPress>", self.change_xy)            
 
 root = Tk()
-canv = Canvas(root, width=WIDTH, height=HEIGHT, bg="#003300")
-canv.pack()
+newgame = SnakeGame()
+newgame.go()
 
-snake_head = canv.create_oval([240,240],[260,260], fill="white")
-snake_body = []
-apple = canv.create_oval([0,20],[20,40], fill="white")
-#snake_body.append(canv.create_oval([240,260],[260,280], fill="white"))
-#snake_body.append(canv.create_oval([240,280],[260,300], fill="white"))
-#snake_body.append(canv.create_oval([240,300],[260,320], fill="white"))
-#snake_body.append(canv.create_oval([240,320],[260,340], fill="white"))
-root.bind("<KeyPress>", change_xy)
-
-go()
-#im ='/Users/irina_dashevskaya/project/example.gif'
-#ph_im =PhotoImage(file=im)
-#canv.create_image(1,1,anchor=NW,image=ph_im)
 root.mainloop()
